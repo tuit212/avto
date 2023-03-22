5ab985bb394c9c5f9404a4764fb295fcc9bdcab1
tuit212 <asadbekanvarov9808@example.com>
2023-03-22 22:55:11 +0500

    end footer

--- src/Pages/Detals/Detals.jsx
+++ src/Pages/Detals/Detals.jsx
@@ -7,18 +7,19 @@ const Detals = ({item , handleClick}) => {
   const {id , images , title , desc } = item;
   return (
     <div className='pages-context'>
-        <Link className='link' to={`${id}`} >
+        <div className='link'  >
             <div className="pages__context-top">
               <img className='pages__img' src={images} alt={title} />
             </div>
             <div className="pages__context">
               <h4>{title}</h4>
               <p>{desc}</p>
+              <Link className='more'  to={`${id}`} > More </Link>
             </div>
             <div className="pages__btn">
               <button onClick={() => handleClick(item)}>Add Cart</button>
             </div>
-        </Link>
+        </div>
     </div>
   )
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react' 
+import './Footer.scss'
+import { Link , NavLink} from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-section'>
+      <div className="container">
+        <div className="footer">
+          <div className="footer__logo">
+            <Link to={'/'} className='logo' >Logo</Link>
+          </div>
+          <div className="footer__navbar">
+            <NavLink to={'about'} className='navbar'>
+              About
+            </NavLink>
+            <NavLink to={'contact'} className='navbar'>
+              Contact
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
